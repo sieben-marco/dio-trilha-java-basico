4bae77421c28de4df87f403ace41148fab9f2fc9
sieben-marco <57383943+sieben-marco@users.noreply.github.com>
2025-04-27 02:07:51 -0300

    concluir projeto dio terminal banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Número conta: ");
+        int numConta = in.nextInt();
+        System.out.print("Agência: ");
+        String agencia = in.next();
+        System.out.print("Nome: ");
+        String nome = in.next();
+        System.out.print("Saldo: ");
+        double saldo = in.nextDouble();
+
+        System.out.printf("Número conta: \t%d%n", numConta);
+        System.out.printf("Agência: \t\t%s%n", agencia);
+        System.out.printf("Nome: \t\t\t%s%n", nome);
+        System.out.printf("Saldo: \t\t\t%.2f%n", saldo);
 
     }
 
